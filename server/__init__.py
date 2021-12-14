93211e76b3312cefee0c2f8ba36356521f9cf4c0
Carlos Estrada <carlos.deg02@hotmail.com>
2021-12-14 21:32:57 +0000

    Reformatted server to own directory

--- server/__init__.py
+++ server/__init__.py
@@ -5,12 +5,16 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
-    SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'),
+        SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'),
     )
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello World'
 
     return app
